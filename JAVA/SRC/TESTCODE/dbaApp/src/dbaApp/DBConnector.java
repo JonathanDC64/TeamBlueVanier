2e6078050f0acc7bd27a51a8f9bd4f6b0ab9d749
EpicNameBro <nintendo_jon64@hotmail.com>
2015-01-27 09:27:41 -0500

    updated code
    updated code

--- JAVA/SRC/TESTCODE/dbaApp/src/dbaApp/DBConnector.java
+++ JAVA/SRC/TESTCODE/dbaApp/src/dbaApp/DBConnector.java
@@ -1,6 +1,12 @@
 package dbaApp;
-import java.sql.* ;  // for standard JDBC programs
-import java.math.* ; // for BigDecimal and BigInteger support
+import java.sql.Connection;  // for standard JDBC programs
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+// for BigDecimal and BigInteger support
 public class DBConnector {
 
 	public DBConnector()
@@ -12,7 +18,7 @@ public class DBConnector {
 		
 	}
 	
-	public static String report()
+	public static String[][] report()
 	{
 		try {
 			   Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -42,7 +48,7 @@ public class DBConnector {
 			//stmt.execute(SQL);
 			ResultSet rs;
 			
-			rs = stmt.executeQuery("select * from student");
+			rs = stmt.executeQuery("select * from student order by studentid");
 			
 			//rs.next();
 			
@@ -53,36 +59,81 @@ public class DBConnector {
 			//System.out.println(rs.getString(2));
 			
 			
-			String text = "";
-			
-
+			ArrayList<ArrayList<String>> text = new ArrayList<ArrayList<String>>();
+			int rows = 0;
 			while(rs.next())
 			{
 		         //Retrieve by column name
-		         int id  = rs.getInt("studentid");
+		         String id  = Integer.toString(rs.getInt("studentid"));
 		         String first = rs.getString("fname");
 		         String last = rs.getString("lname");
-		         text += id + " " + first + " " + last + "\n" ;
+		         ArrayList<String> t = new ArrayList<String>();
+		         t.add(id);t.add(first);t.add(last);
+		         text.add(t);
+		         rows++;
 		    }
 
+			String[][] fText = new String[rows][text.get(0).size()];
 			
+			for(int i = 0 ; i < text.size() ; i++)
+			{
+				for(int j = 0 ; j < text.get(0).size() ; j++)
+				{
+					fText[i][j] = text.get(i).get(j);
+				}
+			}
 			
 			conn.close();
 			
-			return text;
+			return fText;
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return "";
+		return null;
 	}
 	
-	
-	public static void main(String[] args) {
+	public static void studentInsert(String id, String fname, String lname)
+	{
+		try {
+			   Class.forName("oracle.jdbc.driver.OracleDriver");
+			}
+			catch(ClassNotFoundException ex) {
+			   System.out.println("Error: unable to load driver class!");
+			   System.exit(1);
+			}
+		String URL = "jdbc:oracle:thin:@localhost:1521:BlueTeam";
+		String USER = "btv";
+		String PASS = "btv";
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(URL, USER, PASS);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
+		PreparedStatement pstmt = null;
 		
+		//String SQL = "insert into student values(1321164,'Joe','Dow')";
+		Statement stmt = null;
+		
+		try {
+			stmt = conn.createStatement( );
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//stmt.execute(SQL);
+		ResultSet rs;
+		try {
+			rs = stmt.executeQuery("insert into student values(" + Integer.parseInt(id) + ",'" + fname + "','" + lname + "')");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- JAVA/SRC/TESTCODE/dbaApp/src/dbaApp/dbaFrame.java
+++ JAVA/SRC/TESTCODE/dbaApp/src/dbaApp/dbaFrame.java
@@ -6,25 +6,40 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
+
 import java.awt.GridLayout;
+
 import javax.swing.JList;
 import javax.swing.JTable;
 
 import java.sql.*;
-
 import java.sql.* ;  // for standard JDBC programs
+import java.util.Arrays;
 import java.math.* ;
+
+import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.JButton;
 import javax.swing.AbstractAction;
+
 import java.awt.event.ActionEvent;
-import javax.swing.Action; // for BigDecimal and BigInteger supp
+
+import javax.swing.Action;
+
+import java.awt.event.ActionListener; // for BigDecimal and BigInteger supp
 
 public class dbaFrame extends JFrame {
 
 	private JPanel contentPane;
 	private final Action action = new SwingAction();
-
+	private JTable table = new JTable();
+	private final JButton btnGo = new JButton("Go!");
+	private JFrame thisFrame = this;
+	private JScrollPane scrollPane = new JScrollPane();
+	
 	/**
 	 * Launch the application.
 	 */
@@ -40,45 +55,50 @@ public class dbaFrame extends JFrame {
 			}
 		});
 	}
-	JTextPane textPane = new JTextPane();
 	/**
 	 * Create the frame.
 	 */
 	public dbaFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 533, 407);
+		setBounds(100, 100, 300, 200);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
+		contentPane.add(scrollPane, BorderLayout.CENTER);
 		
-		contentPane.add(textPane, BorderLayout.CENTER);
-		
-		JButton btnGo = new JButton("Go!");
-		btnGo.setAction(action);
 		contentPane.add(btnGo, BorderLayout.SOUTH);
+		
+		btnGo.addActionListener(new SwingAction());
+		
+		this.setResizable(false);
 	}
 	private class SwingAction extends AbstractAction {
 		public SwingAction() {
 			putValue(NAME, "SwingAction");
 			putValue(SHORT_DESCRIPTION, "Some short description");
 		}
-		public void actionPerformed(ActionEvent e) {
-			
-			
-			
+		public void actionPerformed(ActionEvent e) {	
 			new Thread(new Runnable()
 			{
+				public void run() 
+				{
+					String[][] report = DBConnector.report();
 
-				public void run() {
-					String report = DBConnector.report();
-					textPane.setText(report);
+					String[] names = {"student ID","first name","last name"};
+					
+				    table = new JTable(report, names);
+
+				    scrollPane = new JScrollPane(table);
+				    
+				    contentPane.remove(scrollPane);
+				    
+				    contentPane.add(scrollPane, BorderLayout.CENTER);
+				    thisFrame.revalidate();
+				   // thisFrame.repaint();
 				}
-				
-				
 			}).start();
-			
 		}
 	}
 }
